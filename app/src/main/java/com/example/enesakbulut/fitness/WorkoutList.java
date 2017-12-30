4120dda4d7fefcd22679ff9a3080326193eee3a8
enesakbulut <myps3xx@web.de>
2017-12-30 22:47:42 +0100

    Test

--- app/src/main/java/com/example/enesakbulut/fitness/WorkoutList.java
+++ app/src/main/java/com/example/enesakbulut/fitness/WorkoutList.java
@@ -8,6 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -53,6 +54,13 @@ public class WorkoutList extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
     private void workoutClick() {
