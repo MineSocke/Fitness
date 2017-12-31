8d57dc1afe132c2d6511ea01972ee227f3640bf3
enesakbulut <myps3xx@web.de>
2017-12-31 12:08:17 +0100

    Test

--- app/src/main/java/com/example/enesakbulut/fitness/WorkoutList.java
+++ app/src/main/java/com/example/enesakbulut/fitness/WorkoutList.java
@@ -3,6 +3,8 @@ package com.example.enesakbulut.fitness;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,12 +15,13 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.enesakbulut.fitness.Start.WelcomeActivity;
 
 
-public class WorkoutList extends AppCompatActivity {
+public class WorkoutList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     public static final String PREFS_SETUP = "PREFS";
     public int workoutid;
@@ -38,6 +41,8 @@ public class WorkoutList extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ivBizeps = (ImageView) findViewById(R.id.ivBizeps);
         ivAbs = (ImageView) findViewById(R.id.ivAbs);
@@ -52,6 +57,7 @@ public class WorkoutList extends AppCompatActivity {
         }
 
     }
+
 
 
     @Override
@@ -91,4 +97,23 @@ public class WorkoutList extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id== R.id.nav_workout){
+            Toast.makeText(this, "This is Workout", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.nav_progress){
+            Toast.makeText(this, "This is progress", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_music_playlist){
+            Toast.makeText(this, "This is Music-Playlist", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_settings){
+            Toast.makeText(this, "This is Settings", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_bug){
+            Toast.makeText(this, "This is FoundABug?", Toast.LENGTH_SHORT).show();
+        }else if(id == R.id.nav_rating){
+            Toast.makeText(this, "This is Rating", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
 }
