8b8b65bbcac13774cdb5935ea512b33c0787b436
enesakbulut <myps3xx@web.de>
2018-01-14 16:04:29 +0100

    Test

--- app/src/main/java/com/example/enesakbulut/fitness/Start/SplashActivity.java
+++ app/src/main/java/com/example/enesakbulut/fitness/Start/SplashActivity.java
@@ -30,19 +30,6 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 3000);
 
-        changeStatusBarColor();
-    }
-
-
-    /**
-     * Making notification bar transparent
-     */
-    private void changeStatusBarColor() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
     }
 
 }
