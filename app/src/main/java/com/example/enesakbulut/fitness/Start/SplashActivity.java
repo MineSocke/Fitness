9163901975c456159cb076a53062d83a9ea3b245
enesakbulut <myps3xx@web.de>
2018-01-15 10:58:45 +0100

    Test

--- app/src/main/java/com/example/enesakbulut/fitness/Start/SplashActivity.java
+++ app/src/main/java/com/example/enesakbulut/fitness/Start/SplashActivity.java
@@ -28,7 +28,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), WorkoutListTwo.class));
                 finish();
             }
-        }, 3000);
+        }, 1000);
 
     }
 
