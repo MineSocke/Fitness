6f48ce64de4b1d4eb27ec18dc71e4fff454d3637
Enes AK <myps3xx@web.de>
2018-04-25 18:12:04 +0200

    Test

--- app/src/main/java/com/example/enesakbulut/fitness/WorkoutFinish.java
+++ app/src/main/java/com/example/enesakbulut/fitness/WorkoutFinish.java
@@ -1,6 +1,7 @@
 package com.example.enesakbulut.fitness;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class WorkoutFinish extends AppCompatActivity {
 
     Button bDone;
@@ -19,6 +22,9 @@ public class WorkoutFinish extends AppCompatActivity {
     WorkoutData workoutData = new WorkoutData();
     private int workoutCounter;
     int workoutid;
+
+    ArrayList<WorkoutProgress> data = new ArrayList<>();
+    WorkoutProgress workoutProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +42,7 @@ public class WorkoutFinish extends AppCompatActivity {
         tvCounter.setText(getResources().getString(R.string.workout_count) + workoutCounter);
         workoutid = getIntent().getIntExtra("workoutid", 0);
 
-        saveData();
+        saveData2();
         bSharePressed();
         bDonePressed();
     }
@@ -71,5 +77,14 @@ public class WorkoutFinish extends AppCompatActivity {
     public void saveData(){
         int progress = workoutData.getProgressList(workoutid);
         workoutData.setProgressList(workoutid, progress+1);
+    }
+    public void saveData2(){
+        SharedPreferences sharedProgresslist = getSharedPreferences("sharedProgresslist", 0);
+        SharedPreferences.Editor editor = sharedProgresslist.edit();
+
+        int progress = sharedProgresslist.getInt(String.valueOf(workoutid), 0);
+        editor.putInt(String.valueOf(workoutid), progress+1);
+        editor.apply();
+
     }
 }
---
+++ app/src/main/java/com/example/enesakbulut/fitness/WorkoutProgress.java
@@ -0,0 +1,24 @@
+package com.example.enesakbulut.fitness;
+
+public class WorkoutProgress {
+    String workoutid;
+    String progress;
+
+    public String getWorkoutid() {
+        return workoutid;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+
+    public void setWorkoutid(String workoutid) {
+        this.workoutid = workoutid;
+    }
+
+}
